bc1b7cc2a2b3db0d50977ccc7432a3a75c9b0e49
swy <minicr1234@gmail.com>
2024-06-18 21:34:03 +0800

    feat: menu api

--- src/Data/Restful/menuAPI.js
+++ src/Data/Restful/menuAPI.js
@@ -6,33 +6,33 @@ const myFetch = utility.myFetch;
 
 // 5 APIs
 const menuAPI = {
-    // 取得所有菜單
+    // 取得所有菜單: done
     getAllMenu: async () => {
         const url = `${BASE_URL}/menu`;
         const result = await myFetch("GET", url, {});
         return result;
     },
-    // 透過 ID 取得菜單
+    // 透過 ID 取得菜單: done
     getMenuById: async (jwtToken, id) => {
         const url = `${BASE_URL}/menu/${id}`;
         const result = await myFetch("GET", url, {}, jwtToken);
         return result;
     },
-    // 新增菜單項目
-    addMenuItem: async (jwtToken, name, price) => {
+    // 新增菜單項目: done
+    addMenuItem: async (jwtToken, name, description, price) => {
         const url = `${BASE_URL}/menu`;
-        const body = { name, price };
+        const body = { name, description, price };
         const result = await myFetch("POST", url, body, jwtToken);
         return result;
     },
-    // 更新菜單項目
-    updateMenuItem: async (jwtToken, id, name, price) => {
+    // 更新菜單項目: done
+    updateMenuItem: async (jwtToken, id, name, description, price) => {
         const url = `${BASE_URL}/menu/${id}`;
         const body = { name, price };
         const result = await myFetch("PUT", url, body, jwtToken);
         return result;
     },
-    // 刪除菜單項目
+    // 刪除菜單項目: done
     deleteMenuItem: async (jwtToken, id) => {
         const url = `${BASE_URL}/menu/${id}`;
         const result = await myFetch("DELETE", url, {}, jwtToken);
